23f73f0d105a425880a4c3fe92e698ab3b183eef
alexsouchereau <W0632604@myscc.ca>
2023-02-16 23:14:19 -0500

    Updated index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,6 +5,9 @@
     <h2>Servers</h2>
     <div>
         <!-- TODO Display all groups here with dropdown for contained entries -->
+        <!-- Filter by all/disabled/enabled -->
+        <!-- Sort by edit/creation date -->
+        <!-- Search by address, host, nickname, or group name -->
     </div>
 </section>
 
