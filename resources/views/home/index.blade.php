2c029ec90c0f69eb6eb3e8cdb0b796c4d575804b
alexsouchereau <W0632604@myscc.ca>
2023-02-17 11:34:41 -0500

    Updated index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,6 +8,15 @@
         <!-- Filter by all/disabled/enabled -->
         <!-- Sort by edit/creation date -->
         <!-- Search by address, host, nickname, or group name -->
+
+        @foreach($groups as $group)
+            {{$group->name}} <br>
+            Routes:
+            @foreach($group->routes() as $route)
+            {{$route->nickname}}
+            {{$route->host}}
+            @endforeach
+        @endforeach
     </div>
 </section>
 
