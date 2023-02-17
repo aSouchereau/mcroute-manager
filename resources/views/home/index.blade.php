0a977c8dd3e54bffea9c2ed8a5863c72072967f0
alexsouchereau <W0632604@myscc.ca>
2023-02-16 23:04:20 -0500

    Updated index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,14 @@
 @extends('master')
 @section('content')
-<h2>this is part of the child page</h2>
+<h1>Hello <!-- TODO <username here> --></h1>
+<section>
+    <h2>Servers</h2>
+    <div>
+        <!-- TODO Display all groups here with dropdown for contained entries -->
+    </div>
+</section>
+
+
+
+
 @endsection
