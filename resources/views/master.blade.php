6ec761778612b6fe56f2a57aaa66deb2a4cf8ee7
alexsouchereau <W0632604@myscc.ca>
2023-02-27 15:01:22 -0500

    Added tailwind to project

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,11 +4,15 @@
         <meta charset="utf-8">
         <title>MC Route Manager</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
     </head>
     <body>
         <header>
+            <div class="header-content">
+
+            </div>
         </header>
-        <main>
+        <main class="text-3xl">
             @yield('content')
         </main>
         <footer>
