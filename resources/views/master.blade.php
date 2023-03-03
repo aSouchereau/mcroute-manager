f935dfb27cd2027da1b846ed87e81f1f0f6bc73d
alexsouchereau <W0632604@myscc.ca>
2023-03-03 18:43:08 -0500

    Created and styled header

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,15 +4,26 @@
         <meta charset="utf-8">
         <title>MC Route Manager</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
+        <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+        <link rel="stylesheet" href="{{asset('css/colors.css')}}">
+        <link rel="stylesheet" href="{{asset('css/header.css')}}">
     </head>
-    <body>
+    <body style="margin: 0; background-color: var(--bg-light-grey);">
         <header>
-            <div class="header-content">
-
-            </div>
+            <nav id="main-nav">
+                <a id="main-nav-logo" href="{{route('index')}}">
+                    MC Route Manager
+                </a>
+                <div id="main-nav-list">
+                    <a href="{{route('index')}}" class="main-nav-item">Home</a>
+                    <a href="{{route('index')}}" class="main-nav-item">Logs</a>
+                    <a href="{{route('index')}}" class="main-nav-item">Settings</a>
+                    <a href="{{route('index')}}" class="main-nav-item">Logout</a>
+                </div>
+            </nav>
         </header>
-        <main class="text-3xl">
+
+    <main>
             @yield('content')
         </main>
         <footer>
