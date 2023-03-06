664b53a0ec7cdc53c49d16cc8bf47a36be912d39
alexsouchereau <W0632604@myscc.ca>
2023-03-06 15:15:17 -0500

    Updated layout of group index page

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -5,12 +5,30 @@
 @endsection
 @section('content')
     <x-home-nav activeElm="2"></x-home-nav>
-    <h2>All Groups</h2>
-    @foreach ($groups as $group)
-        Group ID: {{ $group->id }}<br>
-        Group Name: {{ $group->name }}<br>
-        Description: {{ $group->description }}<br>
-        <a href="{{ action([\App\Http\Controllers\GroupController::class, 'edit'], $group->id) }}">[edit]</a><br>
-        <br>
-    @endforeach
+    <div>
+        <h1>Groups</h1>
+        <p>Group routes for easier management</p>
+    </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>description</th>
+                    <th>routes</th>
+                    <th>enabled</th>
+                    <th>actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($groups as $group)
+                <tr>
+                    <td>{{$group->name}}</td>
+                    <td>{{$group->description}}</td>
+                    <td>{{$group->routes->count()}}</td>
+                    <td>enable</td>
+                    <td><a href="{{ action([\App\Http\Controllers\GroupController::class, 'edit'], $group->id) }}">[edit]</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
 @endsection
