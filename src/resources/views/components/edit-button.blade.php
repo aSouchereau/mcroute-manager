cd8878bb208c20958b98a188163941d83491fb40
alexsouchereau <W0632604@myscc.ca>
2023-05-29 18:29:14 -0400

    Created edit button component

---
+++ src/resources/views/components/edit-button.blade.php
@@ -0,0 +1,5 @@
+<button type="button" class="btn btn-stealth" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    <a href="#" data-bs-toggle="tooltip" data-bs-title="Edit {{$name}}" data-bs-placement="bottom">
+        <x-icon name="pencil-simple"></x-icon>
+    </a>
+</button>
