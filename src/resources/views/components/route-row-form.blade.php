0a3ba98d66729513dfe96408560fbbe404d9ea2a
alexsouchereau <W0632604@myscc.ca>
2023-06-17 22:56:26 -0400

    Made component reusable between edit and create

--- src/resources/views/components/route-row-form.blade.php
+++ src/resources/views/components/route-row-form.blade.php
@@ -1,55 +1,64 @@
 <tr>
     <td>
-        <form id="row-form-{{$route->id}}" method="POST" action="{{ route('routes.update', $route->id) }}">
+        <form
+            id="row-form-{{isset($route->id) ?? "new"}}"
+            method="POST"
+            action="{{ (isset($route->id)) ?
+                (route('routes.update', $route->id))
+                :
+                (route('routes.store')) }}">
             @csrf
             @method('PATCH')
-            <input
+            <input form="row-form-{{isset($route->id) ?? "new"}}"
                 @isset($route)
                     value="{{$route->nickname}}"
                 @endisset
-               class="form-control-plaintext"
-               readonly
-               data-form-id="{{$route->id}}"
+                class="form-control-plaintext"
+                readonly
             />
         </form>
     </td>
     <td>
-        <input form="row-form-{{$route->id}}"
+        <input form="row-form-{{isset($route->id) ?? "new"}}"
                @isset($route)
                    value="{{$route->domain_name}}"
                @endisset
                class="form-control-plaintext"
                readonly
-               data-form-id="{{$route->id}}"
         />
     </td>
     <td>
-        <input form="row-form-{{$route->id}}"
+        <input form="row-form-{{isset($route->id) ?? "new"}}"
                @isset($route)
                    value="{{$route->host}}"
                @endisset
                class="form-control-plaintext"
                readonly
-               data-form-id="{{$route->id}}"
         />
     </td>
     <td>
-        <input form="row-form-{{$route->id}}"
+        <input form="row-form-{{isset($route->id) ?? "new"}}"
                @isset($route)
                    value="{{$route->group_id}}"
                @endisset
                class="form-control-plaintext"
                readonly
-               data-form-id="{{$route->id}}"
         />
     </td>
 
-    <td>@if($route->enabled) enabled @else disabled @endif</td>
-
+    @isset($route)
+        <td>
+                @if($route->enabled)
+                    enabled
+                @else
+                    disabled
+                @endif
+        </td>
+    @endisset
 
     <td>
         @isset($route)
-            <x-edit-button tooltipName="Route" fieldId="{{$route->id}}" ></x-edit-button>
+            <x-edit-button tooltipName="Route" fieldId="{{$route->id}}"></x-edit-button>
         @endisset
         <input type="submit" value="Submit"><br>
     </td>
