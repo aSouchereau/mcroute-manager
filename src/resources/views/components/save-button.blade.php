1c526f07aa949cbf19d964f93b0e8cec5d98d7ea
aSouchereau <alex@souch.ca>
2023-08-15 11:44:03 -0400

    Fixed save button submitting wrong form

--- src/resources/views/components/save-button.blade.php
+++ src/resources/views/components/save-button.blade.php
@@ -1,9 +1,5 @@
-<form action="{{route($formRoute, $resource->id)}}" method="post">
-    @csrf
-    @method('PATCH')
-    <button type="submit" class="btn btn-stealth" data-field-editor="save-button" data-target-form-id="{{$fieldId}}">
+<button type="submit" class="btn btn-stealth" data-field-editor="save-button" data-target-form-id="{{$fieldId}}" form="row-form-{{$fieldId ?? "new"}}">
         <a href="#" data-bs-toggle="tooltip" data-bs-title="Save Changes" data-bs-placement="bottom">
             <x-icon name="floppy-disk"></x-icon>
         </a>
-    </button>
-</form>
+</button>
