5d7bd318a7a0119728d457d2089017b1bccb82d0
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-12-13 11:29:54 -0500

    V0.1.10 (#29)
    * Sync job (#25)
    
    * Changed deleteRoute function parameters to use domain name string
    
    * Fixed getActiveRoutes not sending request with json headers
    
    * Changed default queue connection to database
    
    * Created migration for jobs table
    
    * Created controller for manually running sync job
    
    * Created route for manually running sync job
    
    * Created sync job class
    
    * Implemented sync job logic
    
    * Changed invoke method to named method because it didnt work (this one does)
    
    * Added request headers to addRoute
    
    * Added temporary manual sync button
    
    * Fixed sync route to use new method name
    
    * Fixed syncRouter logic to make request with correctly formatted data
    
    * Removed temporary supervisor event listener that wasn't working
    
    * Scheduled sync job
    
    * I somehow didnt remove entire entry the first time
    
    * Created job for resetting router
    
    * Changed sync schedule
    
    * Created controller and route for reset job
    
    * Added temporary reset button
    
    * Added redirect response to manual sync
    
    * Removed discard parameter
    
    * Split sync into separate functions
    
    * Fixed sync job creating route with incorrect host
    
    * Changed try catch to use base exception class
    
    * Changed replaceRoute parameters to not require eloquent model
    
    * Added function to update host values
    
    * Removed redundant reset feature
    
    ---------
    
    Co-authored-by: aSouchereau <alex@souch.ca>
    
    * 27 job automation (#28)
    
    * Updated gitignore
    
    * Added cron schedule
    
    * Added commands to set suid for cron and copy cron schedule
    
    * Changed to run start script as program
    
    * Reduced startsecs to reasonable time
    
    * Added npm and dependency install to dockerfile
    
    * Added command to build for prod on first startup
    
    * Added config for cron and queue worker
    
    * Removed unnecessary output redirection
    
    * Removed user switch to run container as root (temporary solution)
    
    * Create non-root user for processes that require it
    
    * Switched to non-root user in config
    
    * Added user definition to silence warning
    
    * Updated cron command
    
    * Removed line to set cron schedules (moved to dockerfile)
    
    * Changed app directory
    
    ---------
    
    Co-authored-by: aSouchereau <alex@souch.ca>
    
    ---------
    
    Co-authored-by: aSouchereau <alex@souch.ca>

--- src/app/Http/Controllers/RouteController.php
+++ src/app/Http/Controllers/RouteController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Group;
 use App\Models\Route;
 use App\Traits\RouteTrait;
-use GuzzleHttp\Exception\ClientException;
-use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\HttpClientException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class RouteController extends Controller
@@ -44,7 +42,7 @@ class RouteController extends Controller
         // Makes api call to router to activate new route
         try {
             $this->addRoute($route->domain_name, $route->host);
-        } catch (RequestException $e) {
+        } catch (HttpClientException $e) {
             notyf()->addError('Router API: Failed to add new route - ' . $e->getMessage());
             return redirect('routes');
         }
@@ -61,14 +59,13 @@ class RouteController extends Controller
 
     /**
      * Update the specified resource in storage.
-     * @throws Exception
      */
     public function update(Request $request, Route $route): RedirectResponse
     {
         $formData = $request->all();
         try {
-            $this->replaceRoute($route, $request);
-        } catch (RequestException $e) {
+            $this->replaceRoute($route->domain_name, $request->host);
+        } catch (HttpClientException $e) {
             notyf()->addError('Router API: Failed to update route - ' . $e->getMessage());
             return redirect('routes');
         }
@@ -84,8 +81,8 @@ class RouteController extends Controller
     {
         $route = Route::findOrFail($route->id);
         try {
-            $this->deleteRoute($route);
-        } catch (RequestException $e) {
+            $this->deleteRoute($route->domain_name);
+        } catch (HttpClientException $e) {
             notyf()->addError('Router API: Failed to delete route - ' . $e->getMessage());
             return redirect('routes');
         }
@@ -100,7 +97,7 @@ class RouteController extends Controller
 
         try {
             $this->toggleRoute($route);
-        } catch (RequestException $e) {
+        } catch (HttpClientException $e) {
             notyf()->addError('Router API: Failed to toggle route status - ' . $e->getMessage());
             return redirect('routes');
         }
