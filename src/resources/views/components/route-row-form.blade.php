bf51bee71e812e5929b6359eac4a9c86d97a1676
aSouchereau <alex@souch.ca>
2023-12-21 11:38:28 -0500

    Removed conditionals from edit form

--- src/resources/views/components/route-row-form.blade.php
+++ src/resources/views/components/route-row-form.blade.php
@@ -1,70 +1,49 @@
 <tr>
     <form
-        id="row-form-{{$route->id ?? "new"}}"
+        id="row-form-{{$route->id}}"
         method="POST"
-        action="{{ (isset($route->id)) ?
-                (route('routes.update', $route->id))
-                :
-                (route('routes.store')) }}">
-        @csrf
+        action="{{(route('routes.update', $route->id))}}">
         @method('PATCH')
+        @csrf
     </form>
     <td>
-        <input form="row-form-{{$route->id ?? "new"}}"
+        <input form="row-form-{{$route->id}}"
             id="nickname"
             name="nickname"
-            @isset($route)
-                value="{{$route->nickname}}"
-                data-form-id="{{$route->id}}"
-            @endisset
+            value="{{$route->nickname}}"
+            data-form-id="{{$route->id}}"
             class="form-control-plaintext"
             readonly
                disabled
         />
     </td>
     <td>
-        <input form="row-form-{{$route->id ?? "new"}}"
+        <input form="row-form-{{$route->id}}"
                id="domain_name"
                name="domain_name"
-               @isset($route)
-                   value="{{$route->domain_name}}"
-                   data-form-id="{{$route->id}}"
-               @endisset
+               value="{{$route->domain_name}}"
+               data-form-id="{{$route->id}}"
                class="form-control-plaintext"
                readonly
                disabled
         />
     </td>
     <td>
-        <input form="row-form-{{$route->id ?? "new"}}"
+        <input form="row-form-{{$route->id}}"
                id="host"
                name="host"
-               @isset($route)
-                   value="{{$route->host}}"
-                   data-form-id="{{$route->id}}"
-               @endisset
+               value="{{$route->host}}"
+               data-form-id="{{$route->id}}"
                class="form-control-plaintext"
                readonly
                disabled
         />
     </td>
     <td>
-{{--        <input form="row-form-{{$route->id ?? "new"}}"--}}
-{{--               id="group_id"--}}
-{{--               name="group_id"--}}
-{{--               @isset($route)--}}
-{{--                   value="{{$route->group_id}}"--}}
-{{--                   data-form-id="{{$route->id}}"--}}
-{{--               @endisset--}}
-{{--               class="form-control-plaintext"--}}
-{{--               readonly--}}
-{{--        />--}}
-        <select form="row-form-{{$route->id ?? "new"}}"
+        <select form="row-form-{{$route->id}}"
                 id="group_id"
                 name="group_id"
-                @isset($route)
-                    data-form-id="{{$route->id}}"
-                @endisset
+                data-form-id="{{$route->id}}"
                 class="form-select form-control-plaintext"
                 readonly
                 disabled
@@ -72,44 +51,33 @@
             <option value=""></option>
             @foreach($groups as $group)
                 <option value="{{$group->id}}"
-                    @isset($route)
                         @if($group->id === $route->group_id)
                             selected
                        @endif
-                    @endisset>{{$group->name}}</option>
+                >{{$group->name}}</option>
             @endforeach
         </select>
     </td>
 
-    @isset($route)
-        <td>
-            <div class="toggle-switch-wrapper">
-                <x-toggle-switch
-                    routeName="routes.toggle"
-                    :resource="$route"
-                />
-            </div>
-        </td>
-    @endisset
-
     <td>
-        @isset($route)
-            <div data-field-editor="default-button-set" data-form-id="{{$route->id}}">
-                <x-edit-button tooltipName="Route" fieldId="{{$route->id}}"></x-edit-button>
-                <x-delete-button tooltipName="Route" fieldId="{{$route->id}}" :resource="$route"></x-delete-button>
-            </div>
-            <div data-field-editor="edit-button-set" data-form-id="{{$route->id}}" style="display: none">
-                <x-save-button fieldId="{{$route->id}}" :resource="$route"></x-save-button>
-                <x-cancel-button fieldId="{{$route->id}}"></x-cancel-button>
-            </div>
-        @endisset
+        <div class="toggle-switch-wrapper">
+            <x-toggle-switch
+                routeName="routes.toggle"
+                :resource="$route"
+            />
+        </div>
     </td>
 
-    @if(!$route)
-        <td>
-            <x-create-button name="Route" ></x-create-button>
-        </td>
-    @endif
+    <td>
+        <div data-field-editor="default-button-set" data-form-id="{{$route->id}}">
+            <x-edit-button tooltipName="Route" fieldId="{{$route->id}}"></x-edit-button>
+            <x-delete-button tooltipName="Route" fieldId="{{$route->id}}" :resource="$route"></x-delete-button>
+        </div>
+        <div data-field-editor="edit-button-set" data-form-id="{{$route->id}}" style="display: none">
+            <x-save-button fieldId="{{$route->id}}" :resource="$route"></x-save-button>
+            <x-cancel-button fieldId="{{$route->id}}"></x-cancel-button>
+        </div>
+    </td>
 
 
     @if ($errors->any())
