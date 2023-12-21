36b6e369e014ff60a6409ab355afadef70eb15f3
aSouchereau <alex@souch.ca>
2023-12-21 11:42:57 -0500

    Added target attribute to create button so field editor can toggle correct form

--- src/resources/views/components/create-button.blade.php
+++ src/resources/views/components/create-button.blade.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-stealth" data-field-editor="create-button">
+<button type="button" class="btn btn-stealth" data-field-editor="create-button" data-target-form-id="new">
     <a href="#" data-bs-toggle="tooltip" data-bs-title="New {{$name}}" data-bs-placement="bottom">
         <x-icon name="plus-circle"></x-icon>
     </a>
