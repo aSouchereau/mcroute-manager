be3938543893e047d66d7f09670f0e745eae98eb
aSouchereau <alex@souch.ca>
2023-12-21 16:35:54 -0500

    Removed conditionals from edit form

--- src/resources/views/components/group-row-form.blade.php
+++ src/resources/views/components/group-row-form.blade.php
@@ -1,67 +1,54 @@
 <tr>
     <form
-        id="row-form-{{$group->id ?? "new"}}"
+        id="row-form-{{$group->id}}"
         method="POST"
-        action="{{ (isset($group->id)) ?
-                (route('groups.update', $group->id))
-                :
-                (route('groups.store')) }}">
+        action="{{route('groups.update', $group->id)}}">
         @csrf
         @method('PATCH')
     </form>
     <td>
-        <input form="row-form-{{$group->id ?? "new"}}"
+        <input form="row-form-{{$group->id}}"
                id="name"
                name="name"
-               @isset($group)
-                   value="{{$group->name}}"
+               value="{{$group->name}}"
                data-form-id="{{$group->id}}"
-               @endisset
                class="form-control-plaintext"
                readonly
                disabled
         />
     </td>
     <td>
-        <input form="row-form-{{$group->id ?? "new"}}"
+        <input form="row-form-{{$group->id}}"
                id="description"
                name="description"
-               @isset($group)
-                   value="{{$group->description}}"
+               value="{{$group->description}}"
                data-form-id="{{$group->id}}"
-               @endisset
                class="form-control-plaintext"
                readonly
                disabled
         />
     </td>
     <td>
-        @isset($group)
-            {{$group->routes->count()}}
-        @endisset
+        {{$group->routes->count()}}
     </td>
 
-    @isset($group)
-        <td>
-            @if($group->enabled)
-                enabled
-            @else
-                disabled
-            @endif
-        </td>
-    @endisset
+    <td>
+        @if($group->enabled)
+            enabled
+        @else
+            disabled
+        @endif
+    </td>
 
     <td>
-        @isset($group)
-            <div data-field-editor="default-button-set" data-form-id="{{$group->id}}">
-                <x-edit-button tooltipName="Group" fieldId="{{$group->id}}"></x-edit-button>
-                <x-delete-button tooltipName="Group" fieldId="{{$group->id}}" :resource="$group"></x-delete-button>
-            </div>
-            <div data-field-editor="edit-button-set" data-form-id="{{$group->id}}" style="display: none">
-                <x-save-button fieldId="{{$group->id}}" :resource="$group"></x-save-button>
-                <x-cancel-button fieldId="{{$group->id}}"></x-cancel-button>
-            </div>
-        @endisset
+        <div data-field-editor="default-button-set" data-form-id="{{$group->id}}">
+            <x-edit-button tooltipName="Group" fieldId="{{$group->id}}"></x-edit-button>
+            <x-delete-button tooltipName="Group" fieldId="{{$group->id}}" :resource="$group"></x-delete-button>
+        </div>
+        <div data-field-editor="edit-button-set" data-form-id="{{$group->id}}" style="display: none">
+            <x-save-button fieldId="{{$group->id}}" :resource="$group"></x-save-button>
+            <x-cancel-button fieldId="{{$group->id}}"></x-cancel-button>
+        </div>
     </td>
 
 
