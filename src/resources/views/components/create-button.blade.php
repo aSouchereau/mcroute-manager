6b8183828c0275aef7205e6cf35e99cc25c832bd
aSouchereau <alex@souch.ca>
2023-12-21 16:28:14 -0500

    Changed create button to be more semantic

--- src/resources/views/components/create-button.blade.php
+++ src/resources/views/components/create-button.blade.php
@@ -1,5 +1,12 @@
-<button type="button" class="btn btn-stealth" data-field-editor="create-button" data-target-form-id="new">
-    <a href="#" data-bs-toggle="tooltip" data-bs-title="New {{$name}}" data-bs-placement="bottom">
-        <x-icon name="plus-circle"></x-icon>
-    </a>
-</button>
+<a type="button"
+   class="btn btn-stealth"
+   data-field-editor="create-button"
+   data-target-form-id="new"
+   href="#row-form-new"
+   data-bs-toggle="tooltip"
+   data-bs-title="New {{$name}}"
+   data-bs-placement="bottom"
+   style="display: block; width: 100%">
+        <x-icon name="plus-circle" style="width: 1rem"></x-icon>
+        New Route
+</a>
