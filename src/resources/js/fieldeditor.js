7ecee9611a41d89e21bd1eac55e2ac3988a5d45d
aSouchereau <alex@souch.ca>
2024-01-23 14:57:11 -0500

    Created parent function for validating form data

--- src/resources/js/fieldeditor.js
+++ src/resources/js/fieldeditor.js
@@ -48,3 +48,34 @@ function toggleButtonSet(formId) {
         }
     });
 }
+
+/* *****************************
+         Form Validation
+*********************************/
+const saveBtnList = document.querySelectorAll('[data-field-editor="save-button"]');
+
+saveBtnList.forEach( (btn) => {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        validateFormData(btn.getAttribute('form'));
+    });
+});
+
+function validateFormData(formId) {
+    console.log(formId);
+    let validationStatusArr = [
+        validateDomain(formId),
+        validateHost(formId),
+        validateName(formId),
+        validateDescription(formId)
+    ]
+    let firstError = validationStatusArr.find(obj => obj && obj.status === false);
+    if (firstError) {
+        // call function to show error message under input box
+        console.log(firstError.errorMsg);
+    }
+    else {
+        console.log("Form Submitted");
+        document.getElementById(formId).submit();
+    }
+}
