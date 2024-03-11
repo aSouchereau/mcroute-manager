d49522245d507bae97176fb03047ee8240923b9c
aSouchereau <dev@souch.ca>
2024-03-11 13:34:08 -0400

    Added login page

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+<form method="POST" action="{{ route('login.post') }}">
+    @csrf
+    <div>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" value="{{ old('email') }}" required>
+    </div>
+    @error('email')
+        <span>{{ $message }}</span>
+    @enderror
+    <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" autocomplete="current-password">
+    </div>
+    <div>
+        <input type="checkbox" id="remember" name="remember">
+        <label for="remember">Remember Me</label>
+    </div>
+
+    <button type="submit">Login</button>
+</form>
