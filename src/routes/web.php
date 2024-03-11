257a6b995c69c998ca7183f341211d7899dbc20b
aSouchereau <dev@souch.ca>
2024-03-11 13:34:01 -0400

    Added login route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\GroupController;
-use App\Http\Controllers\ResetRouterController;
 use App\Http\Controllers\RouteController;
 use App\Http\Controllers\SyncRouterController;
 use Illuminate\Support\Facades\Route;
@@ -19,14 +19,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RouteController::class, 'index'])->name('index');
 
-Route::group(['as' => 'groups.', 'prefix' => 'groups'], function () {
+Route::get('login', [LoginController::class, 'getLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login'])->name('login.post');
+
+
+Route::group(['as' => 'groups.', 'prefix' => 'groups', 'middleware' => ['auth']], function () {
    Route::post('', [GroupController::class, 'store'])->name('store');
    Route::patch('{group}', [GroupController::class, 'update'])->name('update');
    Route::delete('{group}', [GroupController::class, 'destroy'])->name('delete');
    Route::get('', [GroupController::class, 'index'])->name('index');
 });
 
-Route::group(['as' => 'routes.', 'prefix' => 'routes'], function () {
+Route::group(['as' => 'routes.', 'prefix' => 'routes', 'middleware' => ['auth']], function () {
     Route::patch('{route}/status', [RouteController::class, 'toggle'])->name('toggle');
     Route::post('', [RouteController::class, 'store'])->name('store');
     Route::patch('{route}', [RouteController::class, 'update'])->name('update');
