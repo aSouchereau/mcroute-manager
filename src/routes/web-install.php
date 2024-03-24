7ac775bff9cdc73be2baf337fbdd95416017a4c7
aSouchereau <dev@souch.ca>
2024-03-24 15:41:23 -0400

    Added router config route

--- src/routes/web-install.php
+++ src/routes/web-install.php
@@ -2,8 +2,10 @@
 
 
 use App\Http\Controllers\Installer\InstallerController;
+use App\Http\Controllers\Installer\RouterConnectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'install.', 'prefix' => 'install'], function () {
     Route::get('/', [InstallerController::class, 'welcome'])->name('welcome');
+    Route::get('router', [RouterConnectionController::class, 'view'])->name('router');
 });
