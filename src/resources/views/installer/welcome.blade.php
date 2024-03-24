ab01ecd1e0b31d78a721c5d51cb26212e2a5601a
aSouchereau <dev@souch.ca>
2024-03-24 15:41:12 -0400

    Added begin button

--- src/resources/views/installer/welcome.blade.php
+++ src/resources/views/installer/welcome.blade.php
@@ -1,4 +1,10 @@
 @extends('installer.view')
 @section('installer-content')
     <p>Welcome to McRouteManager</p>
+
+    <div class="installer-buttons">
+        <a href="{{ route('install.router') }}" class="btn btn-success">
+            Begin Setup
+        </a>
+    </div>
 @endsection
