000be7d8b3c104642dd0b47ba075401a64d14b95
aSouchereau <dev@souch.ca>
2024-03-24 14:41:39 -0400

    Moved content to container

--- src/resources/views/installer/view.blade.php
+++ src/resources/views/installer/view.blade.php
@@ -1,5 +1,7 @@
 @extends('master')
 @section('content')
-<h2>Installer</h2>
+<div class="w-50 mx-auto">
+    <h2 class="fw-bold fs-2">Installer</h2>
     @yield('installer-content')
+</div>
 @endsection
