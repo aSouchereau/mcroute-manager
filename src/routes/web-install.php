6931ffb0fb3dd098077901d689a66e5e4a6b1b0f
aSouchereau <dev@souch.ca>
2024-03-24 19:33:28 -0400

    Added migrate route

--- src/routes/web-install.php
+++ src/routes/web-install.php
@@ -2,10 +2,12 @@
 
 
 use App\Http\Controllers\Installer\InstallerController;
+use App\Http\Controllers\Installer\MigrateController;
 use App\Http\Controllers\Installer\RouterConnectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'install.', 'prefix' => 'install'], function () {
     Route::get('/', [InstallerController::class, 'welcome'])->name('welcome');
     Route::get('router', [RouterConnectionController::class, 'view'])->name('router');
+    Route::get('migrate', [MigrateController::class, 'view'])->name('migrate');
 });
