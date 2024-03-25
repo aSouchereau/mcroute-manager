89519326e8b13b5e824a89e1dc5db64fe6ca438a
aSouchereau <dev@souch.ca>
2024-03-25 11:45:10 -0400

    Added registration routes

--- src/routes/web-install.php
+++ src/routes/web-install.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\Installer\InstallerController;
 use App\Http\Controllers\Installer\MigrateController;
+use App\Http\Controllers\Installer\RegisterAdminController;
 use App\Http\Controllers\Installer\RouterConnectionController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,6 @@ Route::group(['as' => 'install.', 'prefix' => 'install'], function () {
     Route::get('/', [InstallerController::class, 'welcome'])->name('welcome');
     Route::get('router', [RouterConnectionController::class, 'view'])->name('router');
     Route::get('migrate', [MigrateController::class, 'view'])->name('migrate');
+    Route::get('register', [RegisterAdminController::class, 'view'])->name('register');
+    Route::post('register', [RegisterAdminController::class, 'register'])->name('register.post');
 });
