9166b2a890c3fc8b812ec2bd3fa3d75e952058fd
aSouchereau <dev@souch.ca>
2024-04-18 18:27:12 -0400

    Refactor success page

--- src/resources/views/installer/success.blade.php
+++ src/resources/views/installer/success.blade.php
@@ -1,12 +1,11 @@
 @extends('installer.view')
 @section('installer-content')
-    <h2>Finalize Installation</h2>
-    <div class="installer-body">
-        <strong>Installation Successful</strong>
-    </div>
+<div>
+    <h2 class="fs-2 mb-5">Installation Successful</h2>
     <div class="installer-buttons">
         <a href="{{ route('routes.index') }}" class="btn btn-success">
             Complete
         </a>
     </div>
+</div>
 @endsection
