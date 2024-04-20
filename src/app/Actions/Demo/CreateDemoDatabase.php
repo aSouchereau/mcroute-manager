9674e91300946e8ea378ad2a3503b1f8558f91b8
aSouchereau <dev@souch.ca>
2024-04-20 16:27:46 -0400

    Fix filepath

--- src/app/Actions/Demo/CreateDemoDatabase.php
+++ src/app/Actions/Demo/CreateDemoDatabase.php
@@ -9,19 +9,17 @@ namespace App\Actions\Demo;
 class CreateDemoDatabase
 {
     private string $DEMO_DATABASE;
-    private string $DEMO_DB_PATH;
     private array $results = [];
     function __construct()
     {
         $this->DEMO_DATABASE = config('database.connections.demo.database');
-        $this->DEMO_DB_PATH = $_SERVER['DOCUMENT_ROOT'] . '/database/' . $this->DEMO_DATABASE;
         $this->results['errors'] = null;
         $this->results['exception'] = null;
     }
 
     public function create(): array {
         try {
-            $file = fopen($this->DEMO_DB_PATH, 'x+');
+            $file = fopen($this->DEMO_DATABASE, 'x+');
             fclose($file);
         } catch (\Exception $e) {
             $this->results['exception'] = $e;
