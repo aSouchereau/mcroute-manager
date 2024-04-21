520dcb55a6fb27180b41aa64b96b805ac6bfaade
aSouchereau <dev@souch.ca>
2024-04-21 11:37:07 -0400

    Fix setup failed message by removing it from view

--- src/resources/views/demo/setup.blade.php
+++ src/resources/views/demo/setup.blade.php
@@ -2,7 +2,6 @@
 @section('demo-setup-content')
     <div>
         <h2 class="fs-2 mb-5">Demo Mode Setup</h2>
-        @if(empty($errors))
             <div class="content card p-3 w-100">
                 <p class="text-success">Setup Successful</p>
             </div>
@@ -11,15 +10,5 @@
                     Next
                 </a>
             </div>
-        @else
-            <div class="installer-body">
-                <strong class="text-danger">Setup Failed.</strong>
-            </div>
-            <div class="installer-buttons">
-                <a href="{{ route('demo.setup') }}" class="btn btn-success">
-                    Retry
-                </a>
-            </div>
-        @endif
     </div>
 @endsection
