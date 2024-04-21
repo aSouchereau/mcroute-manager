0cf63d6eaecaba0a79a49d38d77eb5db60ed65ff
aSouchereau <dev@souch.ca>
2024-04-20 22:13:51 -0400

    Add content to welcome page

--- src/app/Http/Controllers/Demo/DemoController.php
+++ src/app/Http/Controllers/Demo/DemoController.php
@@ -8,6 +8,7 @@ use App\Actions\Demo\SeedDatabase;
 use App\Actions\Installer\Migrator;
 use App\Actions\Installer\RegisterAdmin;
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\Checks\IsInstalled;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -19,21 +20,26 @@ class DemoController extends Controller
     private Migrator $migrator;
     private RegisterAdmin $registerAdmin;
     private SeedDatabase $seedDatabase;
+    private IsInstalled $isInstalled;
     function __construct(
         CreateDemoDatabase $createDemoDatabase,
         Migrator $migrator,
         RegisterAdmin $registerAdmin,
-        SeedDatabase $seedDatabase)
+        SeedDatabase $seedDatabase,
+        IsInstalled $isInstalled)
     {
         $this->createDemoDatabase = $createDemoDatabase;
         $this->migrator = $migrator;
         $this->registerAdmin = $registerAdmin;
         $this->seedDatabase = $seedDatabase;
+        $this->isInstalled = $isInstalled;
     }
 
     public function welcome(): Factory|View|Application
     {
-        return view('demo.welcome');
+        return view('demo.welcome', [
+            'isInstalled' => $this->isInstalled,
+        ]);
     }
 
     public function setup(): Factory|View|Application
--- src/resources/views/demo/welcome.blade.php
+++ src/resources/views/demo/welcome.blade.php
@@ -1,4 +1,27 @@
 @extends('demo.view')
 @section('demo-setup-content')
-{{ \Illuminate\Support\Facades\DB::connection()->getDatabaseName() }}
+    <div class="logo">
+        <img src="/img/logomark.png" alt="mcroutemanager icon logo">
+    </div>
+    <div class="content">
+        <p>Welcome to</p>
+        <p class="fw-bold text-black fs-4 mb-2">MC Route Manager</p>
+        @if($isInstalled)
+            <p class="mb-5">Demo mode is active. The application has already been installed and admin account created.
+                Feel free to add and remove any items you like, the app resets every hour.</p>
+            <div class="installer-buttons">
+                <a href="{{ route('demo.login') }}" class="btn btn-success">
+                    Login
+                </a>
+            </div>
+        @else
+            <p class="mb-5">Demo mode is enabled, but has not been set up yet. Click continue to begin.</p>
+            <div class="installer-buttons">
+                <a href="{{ route('demo.setup') }}" class="btn btn-success">
+                    Continue
+                </a>
+            </div>
+        @endif
+
+    </div>
 @endsection
