bdca698206afee4e1f13bd99b613409bc33fbe54
aSouchereau <dev@souch.ca>
2024-04-26 10:28:57 -0400

    Update register page

--- src/resources/views/installer/register.blade.php
+++ src/resources/views/installer/register.blade.php
@@ -4,14 +4,10 @@
 <form method="POST" action="{{ route('install.register.post') }}" class="w-50 mx-auto mt-4">
     @csrf
     <h1 class="mb-3">Register Admin Account</h1>
-    <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input id="name" type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="Your Name" required autofocus>
-    </div>
 
     <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input id="email" type="email" name="email" class="form-control" placeholder="a123@example.com" value="{{ old('email') }}" required>
+        <label for="username" class="form-label">Username</label>
+        <input id="username" type="username" name="username" class="form-control" placeholder="Username" value="{{ old('username') }}" required>
     </div>
 
     <div class="mb-3">
