ef3be036d37737ec8627e508cffe469369fcb6e3
aSouchereau <dev@souch.ca>
2024-07-31 15:16:50 -0400

    Remove use statement

--- src/app/Jobs/SyncDbRouter.php
+++ src/app/Jobs/SyncDbRouter.php
@@ -8,7 +8,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
