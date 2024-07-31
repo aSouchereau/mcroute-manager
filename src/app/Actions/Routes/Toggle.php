e24d91b236b0f38ea3ccfff99e0edca0b1a43e6d
aSouchereau <dev@souch.ca>
2024-07-31 13:21:50 -0400

    Add functions for explicitly enabling/disabling route

--- src/app/Actions/Routes/Toggle.php
+++ src/app/Actions/Routes/Toggle.php
@@ -9,15 +9,28 @@ use Illuminate\Http\Client\HttpClientException;
 class Toggle
 {
     use RouteTrait;
-    public function toggle(Route $route) {
+    public function toggle(Route $route): void {
         try {
             $this->toggleRoute($route);
         } catch (HttpClientException $e) {
             notyf()->addError('Router API: Failed to toggle route status - ' . $e->getMessage());
-            return redirect('routes');
         }
 
         $route->enabled = !$route->enabled;
+        $route->save();
+    }
+
+    public function setEnabled(Route $route): void {
+        $this->addRoute($route->domain_name, $route->host);
+
+        $route->enabled = true;
+        $route->save();
+    }
+
+    public function setDisabled(Route $route): void {
+        $this->deleteRoute($route->domain_name);
+
+        $route->enabled = false;
         $route->save();
     }
 }
